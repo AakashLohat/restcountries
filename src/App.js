775ba50cf5e8a4da5effd5c402cc25457bc95b79
AakashLohat <lohatsroyal1974@gmail.com>
2021-01-22 23:42:29 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
+  const [country, setCountry] = React.useState('');
+  const [datas, setDatas] = React.useState();
+
+  const main__section = async () => {
+    if (country.length > 0) {
+      const api__key = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+      const data = await fetch(api__key);
+      const resp = await data.json();
+
+      setDatas(resp[0])
+    }
+  }
+  React.useEffect(() => {
+
+    main__section();
+  }, [country])
+
+
+  console.log(datas)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search__top">
+        <input className="input__type" placeholder="enter country name" type="text" onChange={(event) => setCountry(event.target.value)} />
+      </div>
+
+      <div className="main__parties">
+        <div className="name__country">
+          <h2>country:</h2><span>{datas?.name}</span>
+        </div>
+        <div className="borders">
+          <h2>borders:</h2><span>{datas?.borders.map(i => {
+            return (
+              <h6>{i}</h6>
+            )
+          })}</span>
+        </div>
+        <div className="country__code">
+          <h2>country code:</h2><span>{datas?.callingCodes.map(ik => {
+            return (
+              <h4>{ik}</h4>
+            )
+          })}</span>
+        </div>
+        <div className="currency">
+          <h2>currency:</h2><span>{datas?.currencies.map(i => {
+            return (
+              <h4>{i.name}</h4>
+            )
+          })}</span>
+        </div>
+        <div className="languages">
+          <h2>languages:</h2><span>{datas?.languages.map(i => {
+            return (
+              <>
+                <h4>{i.name}</h4>
+
+              </>
+            )
+          })}</span>
+        </div>
+      </div>
     </div>
   );
 }
